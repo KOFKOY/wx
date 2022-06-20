bd0c5b791c0fe369699f7a767ba248fe1b00f24b
王帅杰 <shuaijie.wang@ziel.cn>
2022-06-20 18:17:44 +0800

    TemplateConfig

--- src/main/java/com/wsj/wx/WxApplication.java
+++ src/main/java/com/wsj/wx/WxApplication.java
@@ -1,7 +1,10 @@
 package com.wsj.wx;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class WxApplication {
@@ -10,4 +13,8 @@ public class WxApplication {
         SpringApplication.run(WxApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate initRestTemplate(){
+        return new RestTemplate();
+    }
 }
