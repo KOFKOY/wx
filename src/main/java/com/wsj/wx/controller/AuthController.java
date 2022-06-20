a3ef4c92a3d5b019da15f39104a0fbf23749599a
王帅杰 <shuaijie.wang@ziel.cn>
2022-06-20 16:55:29 +0800

    部署测试

--- src/main/java/com/wsj/wx/controller/AuthController.java
+++ src/main/java/com/wsj/wx/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.wsj.wx.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -12,7 +13,6 @@ import java.util.Map;
  * @date 2022/5/25 21:21
  */
 @RestController
-@RequestMapping("/auth")
 @Slf4j
 public class AuthController {
 
@@ -25,5 +25,14 @@ public class AuthController {
     @GetMapping("/test")
     public String test(){
         return "测试接口Github Actions";
+    }
+
+    @PostMapping
+    public String post(@RequestBody(required = false) HashMap map){
+        log.info("post接口调用");
+        map.keySet().stream().forEach(key -> {
+            log.info("key:{},value:{}",key,map.get(key));
+        });
+        return "接口调用";
     }
 }
